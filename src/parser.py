862b331fcebb60338d5b0e54effb2451b1a3ffe1
Maryam <aminian.mary@gmail.com>
2018-02-28 15:07:05 -0500

    set dev_batch_size in get_batches()

--- src/parser.py
+++ src/parser.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_option("--d_prime_l", type="int", dest="d_prime_l", default=128)
     parser.add_option("--k", type="int", dest="k", default=4)
     parser.add_option("--batch", type="int", dest="batch", default=10000)
-    parser.add_option("--dev_batch_size", type="int", dest="dev_batch_size", default=500)
+    parser.add_option("--dev_batch_size", type="int", dest="dev_batch_size", default=1000)
     parser.add_option("--alpha", type="float", dest="alpha", default=0.25)
     parser.add_option("--beta2", type="float", dest="beta2", default=0.999)
     parser.add_option("--beta1", type="float", dest="beta1", default=0.9)
@@ -80,7 +80,6 @@ if __name__ == '__main__':
             words, lemmas, pos, roles, chars, stored_opt = pickle.load(paramsfp)
         stored_opt.external_embedding = options.external_embedding
         parser = SRLLSTM(words, lemmas, pos, roles, chars, stored_opt)
-        parser.batch_size = options.dev_batch_size
         parser.Load(os.path.join(options.outdir, options.model))
         ts = time.time()
         pred = list(parser.Predict(options.input, options.sen_cut))
@@ -93,7 +92,6 @@ if __name__ == '__main__':
             words, lemmas, pos, roles, chars, stored_opt = pickle.load(paramsfp)
         stored_opt.external_embedding = options.external_embedding
         parser = SRLLSTM(words, lemmas, pos, roles, chars, stored_opt)
-        parser.batch_size = options.dev_batch_size
         parser.Load(os.path.join(options.outdir, options.model))
         ts = time.time()
         for dir, subdir, files in os.walk(options.inputdir):
