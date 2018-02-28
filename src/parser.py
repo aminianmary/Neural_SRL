0110ac5f4d436657ab3ba1611073de5b30d0c852
Maryam <aminian.mary@gmail.com>
2018-02-28 17:40:08 -0500

    small formatting

--- src/parser.py
+++ src/parser.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_option("--k", type="int", dest="k", default=4)
     parser.add_option("--char_k", type="int", dest="char_k", default=1)
     parser.add_option("--batch", type="int", dest="batch", default=10000)
-    parser.add_option("--dev_batch_size", type="int", dest="dev_batch_size", default=1000)
+    parser.add_option("--dev_batch_size", type="int", dest="dev_batch_size", default=200)
     parser.add_option("--alpha", type="float", dest="alpha", default=0.25)
     parser.add_option("--beta2", type="float", dest="beta2", default=0.999)
     parser.add_option("--beta1", type="float", dest="beta1", default=0.9)
