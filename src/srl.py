7e84670aac7571c51eab9a0b620a9cfd5b54e043
Maryam <aminian.mary@gmail.com>
2018-03-02 11:11:10 -0500

    removing senses with different steps from predictions

--- src/srl.py
+++ src/srl.py
@@ -170,6 +170,13 @@ class SRLLSTM:
         offset = 0
         for iSentence, sentence in enumerate(dev_data):
             for p in sentence.predicates:
-                sentence.entries[p].sense = results[offset]
+                prediction = results[offset]
+                prediction_stem = prediction.split('.')[0]
+                word = sentence.entries[p].form.lower()
+                k = min(len(prediction_stem), len(word), 3)
+                if prediction_stem[0:k] != word[0:k]:
+                    sentence.entries[p].sense = word+'.01'
+                else:
+                    sentence.entries[p].sense = prediction
                 offset+=1
             yield sentence
